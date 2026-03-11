a82466a93149a66ad734b81d801437424fc40ce6
sai kiran <saikiran@sais-MacBook-Air.local>
2026-03-11 16:50:54 +0530

    added new changes for watsapp

--- src/services/whatsapp.service.js
+++ src/services/whatsapp.service.js
@@ -13,46 +13,6 @@ const sendReceiptWhatsapp = async (phone, filePath, donorName, amount) => {
   form.append("template_name", "thank_you_page");
   form.append("template_language", "en_GB");
 
-  form.append(
-    "components[]",
-    JSON.stringify({
-      type: "body",
-      parameters: [
-        { type: "text", text: donorName },
-        { type: "text", text: String(amount) }
-      ]
-    })
-  );
-
-  form.append(
-    "header_attachment",
-    fs.createReadStream(filePath)
-  );
-
-  const response = await axios.post(
-    "https://wapi.flaxxa.com/api/v1/sendtemplatemessage_withattachment",
-    form,
-    {
-      headers: {
-        ...form.getHeaders(),
-        Accept: "application/json"
-      }
-    }
-  );const axios = require("axios");
-const FormData = require("form-data");
-const fs = require("fs");
-require("dotenv").config();
-
-const sendReceiptWhatsapp = async (phone, filePath, donorName, amount) => {
-
-  const form = new FormData();
-
-  form.append("token", process.env.FLAXXA_TOKEN);
-  form.append("phone", phone);
-
-  form.append("template_name", "thank_you_page");
-  form.append("template_language", "en_GB");
-
 
   form.append(
     "components",
@@ -89,12 +49,6 @@ const sendReceiptWhatsapp = async (phone, filePath, donorName, amount) => {
       headers: form.getHeaders()
     }
   );
-
-  return response.data;
-};
-
-module.exports = { sendReceiptWhatsapp };
-
 
   return response.data;
 };
