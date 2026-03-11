0e74b52e8c7a8e8c4711796b131d774c97667357
sai kiran <saikiran@sais-MacBook-Air.local>
2026-03-11 16:41:23 +0530

    added new updates

--- src/services/whatsapp.service.js
+++ src/services/whatsapp.service.js
@@ -38,7 +38,63 @@ const sendReceiptWhatsapp = async (phone, filePath, donorName, amount) => {
         Accept: "application/json"
       }
     }
+  );const axios = require("axios");
+const FormData = require("form-data");
+const fs = require("fs");
+require("dotenv").config();
+
+const sendReceiptWhatsapp = async (phone, filePath, donorName, amount) => {
+
+  const form = new FormData();
+
+  form.append("token", process.env.FLAXXA_TOKEN);
+  form.append("phone", phone);
+
+  form.append("template_name", "thank_you_page");
+  form.append("template_language", "en_GB");
+
+
+  form.append(
+    "components",
+    JSON.stringify([
+      {
+        type: "body",
+        parameters: [
+          {
+            type: "text",
+            text: donorName
+          },
+          {
+            type: "text",
+            text: String(amount)
+          }
+        ]
+      }
+    ])
   );
+
+  form.append(
+    "header_attachment",
+    fs.createReadStream(filePath),
+      {
+        filename: "Annadhan_Certificate.pdf",
+        contentType: "application/pdf"
+      }
+  );
+
+  const response = await axios.post(
+    "https://wapi.flaxxa.com/api/v1/sendtemplatemessage_withattachment",
+    form,
+    {
+      headers: form.getHeaders()
+    }
+  );
+
+  return response.data;
+};
+
+module.exports = { sendReceiptWhatsapp };
+
 
   return response.data;
 };
